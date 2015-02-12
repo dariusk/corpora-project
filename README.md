45e638e69dfee3f3e6950b158316db77b1fc62d7
Kazemi, Darius <dkazemi@akamai.com>
2015-02-12 15:24:18 -0500

    first

---
+++ README.md
@@ -0,0 +1,48 @@
+# corpora-project
+
+A tool to grab the latest Corpora Project data locally and access it.
+
+## Getting Started
+Install the module with: `npm install corpora-project`
+
+```javascript
+var corpora = require('corpora-project');
+corpora.getFile('animals', 'common');
+```
+
+## Documentation
+Using the module is pretty easy. If you want to access a corpora file called `nouns.json` in the `words` directory, just call `getCategories('words', 'nouns')`. In more detail:
+
+```javascript
+var corpora = require('corpora-project');
+
+// List all corpora categories
+corpora.getCategories(); // ["animals", "archetypes", ...]
+
+// List all files in a category
+corpora.getFiles('animals'); // ["birds_antarctica", "birds_uk", "common", ...]
+
+// Get the data from a file
+corpora.getFile('animals', 'common');
+
+// returns this object:
+
+```json
+{
+  "animals":
+    [
+      "aardvark",
+      "alligator",
+      "alpaca",
+      //...
+      "zebra"
+    ]
+}
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## License
+Copyright (c) 2015 Kazemi, Darius  
+Licensed under the MIT license.
---
+++ test/corpora_test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var corpora = require('../lib/corpora.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['awesome'] = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'tests': function(test) {
+    test.expect(7);
+    // tests here
+    test.equal(typeof(corpora), 'object', 'should return an object');
+    test.ok(corpora.getCategories().length, 'should return an array (object w length)');
+    test.equal(typeof(corpora.getCategories()[0]), 'string', 'should be a string');
+    var files = corpora.getFiles('animals');
+    test.ok(files.length, 'should return an array (object w length)');
+    test.equal(typeof(files[0].name), 'string', 'should be a string');
+    test.equal(typeof(files[0].get), 'function', 'should have a `get` function');
+    var file = corpora.getFile('animals', 'common');
+    test.equal(typeof(file), 'object', 'should return an object');
+    test.done();
+  },
+};
